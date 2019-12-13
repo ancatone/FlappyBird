3c501882358232948da3ef94efe0a4c91d64561e
liu <xiaoqing@qq.com>
2019-12-13 17:53:34 +0800

    day1pm结束 定义资源数据，定义资源加载器，定义变量池，在Main中初始化数据，加载数据

---
+++ js/base/Resources.js
@@ -0,0 +1,12 @@
+/**
+ *  图片资源文件
+ */
+export const Resources=[
+    ["background","images/background.png"],
+    ["land","images/land.png"],
+    ["birds","images/birds.png"],
+    ["UpPipe","images/pie_up.png"],
+    ["DownPipe","images/pie_down.png"],
+    ["startButton","images/start_button.png"],
+    
+];
